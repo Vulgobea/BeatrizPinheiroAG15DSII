ab4e6fdd827a875d78b38221ccdf58c10b26f5be
Vulgobea <fernandesbeatriz99@gmail.com>
2025-06-01 20:05:26 -0300

    Adicionar arquivos de projeto.

---
+++ BeatrizPinheiroAG15DSII/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BeatrizPinheiroAG15DSII
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BeatrizPinheiroAG15DSII/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BeatrizPinheiroAG15DSII
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
